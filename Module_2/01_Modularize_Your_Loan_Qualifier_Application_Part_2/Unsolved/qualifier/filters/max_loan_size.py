118ea4edcb8b6ffd143c1d00a0502123dbb399e5
Vladislav Glupak <vglupak@gmail.com>
2021-12-20 16:03:51 -0800

    adding working files from Module 2.1 and Module 2.2 lessons

---
+++ Module_2/01_Modularize_Your_Loan_Qualifier_Application_Part_2/Unsolved/qualifier/filters/max_loan_size.py
@@ -0,0 +1,9 @@
+# module filters banks by max loan size
+
+def filter_max_loan_size(loan_amount, bank_list):
+    loan_size_approval_list = []
+
+    for bank in bank_list:
+        if loan_amount <= int(bank[1]):
+            loan_size_approval_list.append(bank)
+    return loan_size_approval_list
