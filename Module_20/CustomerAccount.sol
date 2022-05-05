b5050c468f0a57610de192698ef4573fc9e0cca8
Vladislav Glupak <vglupak@gmail.com>
2022-05-05 10:01:46 -0700

    M 20 1

---
+++ Module_20/CustomerAccount.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract CustomerAccount {
+    address owner = 0x24729BFae855aEB1cC94d9845Ee51d23C4a02713;
+    bool isNewAccount;
+    uint accountBalance = 10000;
+    string customerName = "Alex";
+    string customerLastName = "Habib";
+}
